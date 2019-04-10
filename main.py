8331a6465b9ecb21f7c298638cf365f73146b9c1
jordy <jordy@email.com>
2019-04-10 13:08:34 -0300

    update threads

--- main.py
+++ main.py
@@ -2,83 +2,92 @@
 #encoding: utf-8
 
 import csv,requests,os,sys
+import threading
 from bs4 import BeautifulSoup
 
 MAX_STORES_PER_CARD = 100
 
 STORES = dict()
 
+class card_prices_thread(threading.Thread):
+    def __init__(self,card):
+        threading.Thread.__init__(self)
+        self.card = card
 
-def get_card_price(card):
-    """
-        Gets card price from LIGA MAGIC
-    """
-    card_name = card['name']
-    card_name = card_name.replace(' ','+')
-    response = requests.get('https://www.ligamagic.com.br/?view=cards/card&card=' + card_name)
-    soup = BeautifulSoup(response.text,'html.parser')
-    l = [float(soup.find(id='precos-menor').text[3:].replace(',','.')),\
-        float(soup.find(id='precos-medio').text[3:].replace(',','.')),\
-        float(soup.find(id='precos-maior').text[3:].replace(',','.'))]
-    # get card stores
-    divs = soup.findAll("div",{"class":'estoque-linha'})
-    for stock_div in divs[:MAX_STORES_PER_CARD]:
-        try: 
-            link_for_store = stock_div.find("a",{"class":'goto'}).get('href')
-            response = requests.get('https://www.ligamagic.com.br/' + link_for_store)
-            soup = BeautifulSoup(response.text,'html.parser')
-            table = soup.findAll('table')[9]
-            rows = table.findAll('tr')[1:]
-            for row in rows:
-                # print(response.url.split('?')[0])
-                cells = [x.text for x in row.find_all('td')]
-                if len(cells) < 6 or len(cells) > 7:
-                    continue
-                language = cells[1].replace('\xa0','')
-                #Some sites have a TD with 6 TR, others with 7  
-                if len(cells) == 6:
-                    quantity = int(cells[3].replace(' unid.',''))
-                    try:
-                        price    = float(cells[4].replace('R$ ','').replace(',','.'))
-                    except ValueError:
-                        text = list(filter(None,cells[4].replace('R$ ','').replace(',','.').split('\n')))
-                        price = float(min(text))
-                else:
-                    quantity = int(cells[4].replace(' unid.',''))
-                    try:
-                        price    = float(cells[5].replace('R$ ','').replace(',','.'))
-                    except ValueError:
-                        text = list(filter(None,cells[5].replace('R$ ','').replace(',','.').split('\n')))
-                        price = float(min(text))
-                if quantity >= card['quantity']:
-                    store =response.url.split('?')[0]
-                    card_obj = {card['name']:{\
-                                    'price':price,\
-                                    'quantity':quantity,\
-                                    'total_price':card['quantity']*price \
-                                    }}
-                    if not store in STORES:
-                        STORES[store] = {'cards':{},'total_cards':0,'total_price':0}
-                    if not card['name'] in STORES[store]['cards']:
-                        current_store = STORES[store]
-                        current_store['cards'].update(card_obj)
-                        current_store['total_cards'] += card['quantity']
-                        current_store['total_price'] += card_obj[card['name']]['total_price']
+    def run(self):
+        self.get_card_price(self.card)
+
+    def get_card_price(self,card):
+        global STORES
+        """
+            Gets card price from LIGA MAGIC
+        """
+        card_name = card['name']
+        card_name = card_name.replace(' ','+')
+        response = requests.get('https://www.ligamagic.com.br/?view=cards/card&card=' + card_name)
+        soup = BeautifulSoup(response.text,'html.parser')
+        l = [float(soup.find(id='precos-menor').text[3:].replace(',','.')),\
+            float(soup.find(id='precos-medio').text[3:].replace(',','.')),\
+            float(soup.find(id='precos-maior').text[3:].replace(',','.'))]
+        # get card stores
+        divs = soup.findAll("div",{"class":'estoque-linha'})
+        for stock_div in divs[:MAX_STORES_PER_CARD]:
+            try: 
+                link_for_store = stock_div.find("a",{"class":'goto'}).get('href')
+                response = requests.get('https://www.ligamagic.com.br/' + link_for_store)
+                soup = BeautifulSoup(response.text,'html.parser')
+                table = soup.findAll('table')[9]
+                rows = table.findAll('tr')[1:]
+                for row in rows:
+                    # print(response.url.split('?')[0])
+                    cells = [x.text for x in row.find_all('td')]
+                    if len(cells) < 6 or len(cells) > 7:
+                        continue
+                    language = cells[1].replace('\xa0','')
+                    #Some sites have a TD with 6 TR, others with 7  
+                    if len(cells) == 6:
+                        quantity = int(cells[3].replace(' unid.',''))
+                        try:
+                            price    = float(cells[4].replace('R$ ','').replace(',','.'))
+                        except ValueError:
+                            text = list(filter(None,cells[4].replace('R$ ','').replace(',','.').split('\n')))
+                            price = float(min(text))
                     else:
-                        if current_store['cards'][card['name']]['price'] > price:
-                            current_store[card['name']] = card_obj
-        except KeyboardInterrupt:
-            sys.exit(-1)
-        except:
-            print(f"ERROR FOR: {response.url}")
-            continue
+                        quantity = int(cells[4].replace(' unid.',''))
+                        try:
+                            price    = float(cells[5].replace('R$ ','').replace(',','.'))
+                        except ValueError:
+                            text = list(filter(None,cells[5].replace('R$ ','').replace(',','.').split('\n')))
+                            price = float(min(text))
+                    if quantity >= card['quantity']:
+                        store =response.url.split('?')[0]
+                        card_obj = {card['name']:{\
+                                        'price':price,\
+                                        'quantity':quantity,\
+                                        'total_price':card['quantity']*price \
+                                        }}
+                        if not store in STORES:
+                            STORES[store] = {'cards':{},'total_cards':0,'total_price':0}
+                        if not card['name'] in STORES[store]['cards']:
+                            current_store = STORES[store]
+                            current_store['cards'].update(card_obj)
+                            current_store['total_cards'] += card['quantity']
+                            current_store['total_price'] += card_obj[card['name']]['total_price']
+                        else:
+                            if current_store['cards'][card['name']]['price'] > price:
+                                current_store[card['name']] = card_obj
+            except KeyboardInterrupt:
+                sys.exit(-1)
+            except:
+                print(f"ERROR FOR: {response.url}")
+                continue
 
-        # tr =  soup.find('tr',{'style':'background-color:#F2F2F2'}).find_all('td')
-        # tr =  soup.table.find_all('td')
-        # info = [x.text for x in tr ]
-        # print(info)
-    
-    return l
+            # tr =  soup.find('tr',{'style':'background-color:#F2F2F2'}).find_all('td')
+            # tr =  soup.table.find_all('td')
+            # info = [x.text for x in tr ]
+            # print(info)
+        
+        return l
     
 
 def read_list(deck_file):
@@ -107,12 +116,18 @@ def get_card_prices(deck):
     deck['max'] = 0
     deck['min'] = 0
     deck['med'] = 0
+    threads = []
     for c in deck['cards']:
-        print(f"({deck['cards'].index(c)}/{len(deck['cards'])}) - {c['name']}")
-        c['price']      = get_card_price(c)
-        deck['min'] +=  c['price'][0]*c['quantity']
-        deck['med']  +=  c['price'][1]*c['quantity']
-        deck['max'] +=  c['price'][2]*c['quantity']
+        print(f"({deck['cards'].index(c) + 1}/{len(deck['cards'])}) - {c['name']}")
+        t = card_prices_thread(c)
+        t.start()
+        threads.append(t)
+#        c['price']      = get_card_price(c)
+#        deck['min'] +=  c['price'][0]*c['quantity']
+#        deck['med']  +=  c['price'][1]*c['quantity']
+#        deck['max'] +=  c['price'][2]*c['quantity']
+    for t in threads:
+        t.join()
     return deck
 
 def output_list(deck,filename):
@@ -148,14 +163,14 @@ def output_store_list(stores,filename):
 
 
 def main():
-    if len(sys.argv) < 4 or len(sys.argv) > 4:
+    if len(sys.argv) < 3 or len(sys.argv) > 3:
         print(f'{sys.argv[0]} decklistfile outputfile.csv stores.csv')
         return
     card_list = read_list(sys.argv[1])
     card_list_prices = get_card_prices(card_list)
-    output_list(card_list_prices,sys.argv[2])
+#    output_list(card_list_prices,sys.argv[2])
     # card_stores = get_stores(card_list_prices)
-    output_store_list(STORES,sys.argv[3])
+    output_store_list(STORES,sys.argv[2])
     
 
 if __name__ == "__main__":
